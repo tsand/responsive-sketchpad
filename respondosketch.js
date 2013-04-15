147ea6f84086292a9bf7625ddbdedf89d0d493b4
trsanders <tsanders@iastate.edu>
2013-04-14 22:35:51 -0500

    Store locations as percentage of canvas for easier scaling

--- respondosketch.js
+++ respondosketch.js
@@ -3,48 +3,43 @@ $.fn.respondosketch = function(props) {
     var canvas = this;
     var ctx = $(this)[0].getContext('2d');
 
-    var backgroundColor = props.backgroundColor;
     var aspectRatio = props.aspectRatio;
-
     var sketch = false;
     var drawing = new Array();
-
     var width = canvas.width();
     var height = canvas.height();
-
     var lineColor = 'black';
     var lineSize = 5;
 
+    // Setup Context
+    ctx.lineJoin = 'round';
+    ctx.lineCap = 'round';
+
+    //
     $(window).resize(function(e) {
         var newWidth = canvas.parent().width();
         var newHeight = newWidth / aspectRatio;
-
-        var xScale = newWidth/width;
-        var yScale = newHeight/height;
-
-        for (var i = 0; i < drawing.length; i++) {
-            drawing[i].x *= xScale;
-            drawing[i].y *= yScale;
-        }
 
         setSize(newWidth, newHeight);
 
         redraw();
     });
 
-    var startEvent = 'mousedown touchstart';
+    var startEvent = 'mousedown touchstart ';
     canvas.on(startEvent, function(e) {
         if (e.type == 'touchstart') {
             e.preventDefault();
+        } else {
+            e.originalEvent.preventDefault();
         }
 
         sketch = true;
 
-        var mouse = getMouseLocation(this, e);
+        var canvas = getCanvasLocation(this, e);
 
         drawing.push({
-            x: mouse.x,
-            y: mouse.y,
+            x: canvas.x,
+            y: canvas.y,
             type: e.type,
             color: lineColor,
             size: lineSize
@@ -53,49 +48,65 @@ $.fn.respondosketch = function(props) {
         redraw();
     });
 
-    var moveEvent = 'mousemove touchmove';
+    var moveEvent = 'mousemove touchmove ';
     canvas.on(moveEvent, function(e) {
-        var mouse = getMouseLocation(this, e);
+        var canvas = getCanvasLocation(this, e);
 
         if (sketch) {
             drawing.push({
-                x: mouse.x,
-                y: mouse.y,
+                x: canvas.x,
+                y: canvas.y,
                 type: e.type,
                 color: lineColor,
                 size: lineSize
             });
+
             redraw();
         }
     });
 
-    var endEvent = 'mouseup mouseleave touchend';
+    var endEvent = 'mouseup mouseleave touchend ';
     canvas.on(endEvent, function(e) {
         sketch = false;
     });
 
     function init() {
+
+        var color = props.backgroundColor;
+        var locked = props.locked;
+
+        if (props.json) {
+            drawing = props.json.lines;
+            aspectRatio = props.json.aspectRatio;
+        }
+
+        if (props.locked) {
+            canvas.unbind(startEvent + moveEvent + endEvent);
+        } else {
+            canvas.css('cursor', 'crosshair');
+        }
+
         var width = canvas.parent().width();
         var height = width / aspectRatio;
 
-        ctx.lineJoin = 'round';
-        ctx.lineCap = 'round';
-
-        canvas.css('background-color', backgroundColor);
+        canvas.css('background-color', color);
 
         setSize(width, height);
+
+        redraw();
+
     }
 
-    function getMouseLocation(element, event) {
+    function getCanvasLocation(element, event) {
         if (event.type.indexOf('touch') !== -1) {
             return {
-                x: event.originalEvent.touches[0].pageX - element.offsetLeft,
-                y: event.originalEvent.touches[0].pageY - element.offsetTop
+                x: (event.originalEvent.touches[0].pageX - element.offsetLeft) / width,
+                y: (event.originalEvent.touches[0].pageY - element.offsetTop) / height
             }
         } else {
             return {
-                x: event.pageX - element.offsetLeft,
-                y: event.pageY - element.offsetTop
+                x: (event.pageX - element.offsetLeft) / width,
+                y: (event.pageY - element.offsetTop) / height
             };
         }
     }
@@ -116,13 +127,12 @@ $.fn.respondosketch = function(props) {
         for (var i = 1; i < drawing.length; i++) {
 
             if (drawing[i].type == 'mousemove' || drawing[i].type == 'touchmove') {
-                ctx.moveTo(drawing[i - 1].x, drawing[i - 1].y);
+                ctx.moveTo(drawing[i - 1].x * width, drawing[i - 1].y * height);
             } else {
-                ctx.moveTo(drawing[i].x, drawing[i].y);
+                ctx.moveTo(drawing[i].x * width, drawing[i].y * height);
             }
 
-
-            ctx.lineTo(drawing[i].x, drawing[i].y);
+            ctx.lineTo(drawing[i].x * width, drawing[i].y * height);
             ctx.strokeStyle = drawing[i].color;
             ctx.lineWidth = lineSize
             ctx.stroke()
@@ -130,9 +140,17 @@ $.fn.respondosketch = function(props) {
         ctx.closePath();
     }
 
-    function toJson() {
-        return JSON.stringify(drawing);
-    }
+    this.json = function() {
+        return JSON.stringify({
+            aspectRatio: aspectRatio,
+            lines: drawing
+        });
+    };
+
+    this.img = function() {
+        var img = canvas[0].toDataURL("image/png");
+        return img;
+    };
 
     this.changeLineColor = function(color) {
         lineColor = color;
