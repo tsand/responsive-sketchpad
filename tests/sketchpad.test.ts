4964956c808926deb79f01d3926a6f8266c49541
Theisen Sanders <theisen.sanders@gmail.com>
2020-06-18 20:12:41 +0200

    Format code

--- tests/sketchpad.test.ts
+++ tests/sketchpad.test.ts
@@ -1,8 +1,8 @@
 import Sketchpad from '../src/sketchpad'
 
-describe('Sketchpad', function() {
-    it('should throw an error if not called with an element', function () {
-        // @ts-ignore - allow null for test
-        expect(() => new Sketchpad(null)).toThrow(new Error('Must pass in a container element'));
-    });
-});
+describe('Sketchpad', function () {
+  it('should throw an error if not called with an element', function () {
+    // @ts-ignore: Allow null for test
+    expect(() => new Sketchpad(null)).toThrow(new Error('Must pass in a container element'))
+  })
+})
